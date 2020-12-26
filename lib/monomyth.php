38d66ffa70c89ef971ff54180fb15173ec958a1e
Amit <thecancerus@gmail.com>
2020-12-26 09:48:40 +0530

    added support to enqueue stylesheet for customizing gutenberg editor from site-skin

--- lib/monomyth.php
+++ lib/monomyth.php
@@ -17,6 +17,10 @@ require( 'relative-urls.php' );
 require( 'admin-cleanup.php' ); 
 require( 'class-tgm-plugin-activation.php' ); 
 
+add_action( 'enqueue_block_editor_assets', function() {
+    wp_enqueue_style( 'awesome-editor-styles', site_url('site-skin/css/editor-styles'), false, '1.0', 'all' );
+} );
+
 
 // launching this stuff after theme setup
 add_action( 'after_setup_theme','monomyth_theme_support' );
