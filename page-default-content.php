c7b1ea1c27973eebbd5012a39c92e4e478213598
Amit <thecancerus@gmail.com>
2021-02-16 16:17:54 +0530

    fixed: default page layout was using old way of accessing the core.

--- page-default-content.php
+++ page-default-content.php
@@ -10,19 +10,18 @@ get_header(); ?>
 	<main class="main  col-lg-12 col-md-12 col-sm-12 col-xs-12" role="main">
 		<?php
 		
-
-		$awesome_core=&aw2_library::get_array_ref('awesome_core');
-		
-		if(isset($awesome_core['default-page-layout'])){
-			$layout = $awesome_core['default-page-layout']['code'];
-			unset($awesome_core['default-page-layout']); // now we don't need this data
+		$post_type=AWESOME_CORE_POST_TYPE;
+		$module ='';
+	
+		if(\aw2_library::post_exists('default-page-layout',$post_type)){
+			$module = 'default-page-layout';
 		}
-
-		if(!empty($layout))		
-			echo aw2_library::parse_shortcode($layout);
+		
+		if(!empty($module))		
+			echo \aw2_library::module_run(['post_type'=>$post_type],$module);
 		else
 			echo '<em>default-page-layout</em> is missing.';
-		
+
 		?>
 	</main><!-- /.main -->
 </div><!-- /.content -->
